34e6afa1867c5738482c3b983359d7502fb74e01
Maverick835 <rajmukutg@gmail.com>
2024-04-01 23:02:57 +0530

    4 programs

---
+++ Practical54.py
@@ -0,0 +1,10 @@
+# Read marks of three subjects from the user
+mark1 = float(input("Enter the mark for subject 1: "))
+mark2 = float(input("Enter the mark for subject 2: "))
+mark3 = float(input("Enter the mark for subject 3: "))
+
+# Calculate the average
+average = (mark1 + mark2 + mark3) / 3
+
+# Print the average
+print("The average mark is:", average)
